2e155512862da511620c0a60476f1f96701b96f7
Superjaviergf <superjaviergf@gmail.com>
2020-11-06 18:36:02 +0100

    Servicio de Jugador e implementación. Posibles cambios en el futuro

---
+++ src/main/java/org/springframework/samples/petclinic/service/impl/JugadorServiceImpl.java
@@ -0,0 +1,158 @@
+package org.springframework.samples.petclinic.service.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.enumerate.Posicion;
+import org.springframework.samples.petclinic.model.Jugador;
+import org.springframework.samples.petclinic.repository.JugadorRepository;
+import org.springframework.samples.petclinic.service.JugadorService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("jugadorService")
+public class JugadorServiceImpl implements JugadorService {
+	
+	@Autowired
+	@Qualifier("jugadorRepository")
+	private JugadorRepository jugadorRepository;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private AuthoritiesService authoritiesService;
+	
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findAll() {
+		return jugadorRepository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByFirstName(String name) throws DataAccessException {
+		return jugadorRepository.findByFirstName(name);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPosicionPrincipal(Posicion position) throws DataAccessException {
+		return jugadorRepository.findByPosicionPrincipal(position);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByFechaNacimientoBetweenOrderByFechaNacimiento(LocalDate firstDate, LocalDate secondDate) throws DataAccessException {
+		return jugadorRepository.findByFechaNacimientoBetweenOrderByFechaNacimiento(firstDate, secondDate);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByFechaNacimientoAfterOrderByFechaNacimiento(LocalDate date) throws DataAccessException {
+		return jugadorRepository.findByFechaNacimientoAfterOrderByFechaNacimiento(date);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByAlturaGreaterThanEqual(int height) throws DataAccessException {
+		return jugadorRepository.findByAlturaGreaterThanEqual(height);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByAlturaLessThanEqual(int height) throws DataAccessException {
+		return jugadorRepository.findByAlturaLessThanEqual(height);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPesoGreaterThanEqual(int weight) throws DataAccessException {
+		return jugadorRepository.findByPesoGreaterThanEqual(weight);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPesoLessThanEqual(int weight) throws DataAccessException {
+		return jugadorRepository.findByPesoLessThanEqual(weight);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPorcentajeSaquesLessThanEqual(double percent) throws DataAccessException {
+		return jugadorRepository.findByPorcentajeSaquesLessThanEqual(percent);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPorcentajeRecepcionesLessThanEqual(double percent) throws DataAccessException {
+		return jugadorRepository.findByPorcentajeRecepcionesLessThanEqual(percent);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPorcentajeColocacionesLessThanEqual(double percent) throws DataAccessException {
+		return jugadorRepository.findByPorcentajeColocacionesLessThanEqual(percent);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPorcentajeDefensasLessThanEqual(double percent) throws DataAccessException {
+		return jugadorRepository.findByPorcentajeDefensasLessThanEqual(percent);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPorcentajeBloqueosLessThanEqual(double percent) throws DataAccessException {
+		return jugadorRepository.findByPorcentajeBloqueosLessThanEqual(percent);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPorcentajeRematesLessThanEqual(double percent) throws DataAccessException {
+		return jugadorRepository.findByPorcentajeRematesLessThanEqual(percent);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPorcentajeFintasLessThanEqual(double percent) throws DataAccessException {
+		return jugadorRepository.findByPorcentajeFintasLessThanEqual(percent);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByPorcentajeAtaquesRapidosLessThanEqual(double percent) throws DataAccessException {
+		return jugadorRepository.findByPorcentajeAtaquesRapidosLessThanEqual(percent);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Jugador> findByNumFaltasTotalesGreaterThanEqual(int faults) throws DataAccessException {
+		return jugadorRepository.findByNumFaltasTotalesGreaterThanEqual(faults);
+	}
+
+	@Override
+	@Transactional
+	public void saveJugador(Jugador player) throws DataAccessException {
+		
+		jugadorRepository.save(player);
+		
+		userService.saveUser(player.getUser());
+		
+		authoritiesService.saveAuthorities(player.getUser().getUsername(), "player");
+		
+	}
+
+	@Override
+	@Transactional
+	public List<Jugador> saveEstadisticasJugadores(List<Jugador> players) {
+		
+		List<Jugador> result = (List<Jugador>) jugadorRepository.saveAll(players);
+		
+		return result;
+	}
+
+}
