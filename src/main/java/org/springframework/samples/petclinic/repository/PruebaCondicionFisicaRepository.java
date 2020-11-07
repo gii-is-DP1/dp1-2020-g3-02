37bf1a49faf40e58e0725704841297913e66ce36
Gonzalo <gonlalalv@us.es>
2020-11-07 12:47:17 +0100

    Repositorio Prueba Condición Física

---
+++ src/main/java/org/springframework/samples/petclinic/repository/PruebaCondicionFisicaRepository.java
@@ -0,0 +1,21 @@
+package org.springframework.samples.petclinic.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.samples.petclinic.enumerate.TipoEjercicio;
+import org.springframework.samples.petclinic.enumerate.TipoPrueba;
+import org.springframework.samples.petclinic.model.EjercicioIndividual;
+import org.springframework.samples.petclinic.model.Jugador;
+import org.springframework.samples.petclinic.model.PruebaCondicionFisica;
+
+public interface PruebaCondicionFisicaRepository extends JpaRepository<PruebaCondicionFisica, Serializable> {
+	
+	public List<PruebaCondicionFisica> findById(String id);
+	public List<PruebaCondicionFisica> findByTipoPrueba(TipoPrueba tipo_prueba);
+	public List<PruebaCondicionFisica> findByDatoLessThanEqual(double dato);
+	public List<PruebaCondicionFisica> findByDatoAndTipoPrueba(double dato, TipoPrueba tipo_prueba);
+	public List<PruebaCondicionFisica> findByTipoPruebaAndDatoLessThanEqual(double dato, TipoPrueba tipo_prueba);
+	
+}
