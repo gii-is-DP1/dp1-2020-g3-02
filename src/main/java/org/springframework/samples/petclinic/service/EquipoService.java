7905bf0d653ef3e79c6a88d795d8c5a4991b49e4
DiegoHill <DiegoHill@users.noreply.github.com>
2020-11-11 23:47:19 +0100

    Vistas y controlador de Equipo

--- src/main/java/org/springframework/samples/petclinic/service/EquipoService.java
+++ src/main/java/org/springframework/samples/petclinic/service/EquipoService.java
@@ -1,12 +1,15 @@
 package org.springframework.samples.petclinic.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.samples.petclinic.model.Equipo;
 import org.springframework.samples.petclinic.model.Jugador;
 
 public interface EquipoService {
 	
+	public abstract Optional<Equipo> findById(int id);
+	public abstract List<Equipo> findAll();
 	public abstract Equipo findByCategoria(String category);
 	public abstract List<Equipo> findByCategoriaStartingWith(String category);
 	public abstract List<Equipo> findByLiga(String league);
@@ -19,6 +22,6 @@ public interface EquipoService {
 	public abstract List<Jugador> findByPorcentajeFintasLessThanEqual(double percent);
 	public abstract List<Jugador> findByPorcentajeAtaquesRapidosLessThanEqual(double percent);
 	public abstract List<Jugador> findByNumFaltasTotalesGreaterThanEqual(int faults);
-	public abstract void saveTeam(Equipo team);
+	public abstract Equipo saveTeam(Equipo team);
 
 }
