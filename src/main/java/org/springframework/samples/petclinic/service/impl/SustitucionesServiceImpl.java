b0e5417321d35aaf883f7f2245386e86f6707825
Benjamin <bencrealc@alum.us.es>
2020-11-12 18:30:36 +0100

    Servicio Sustituciones creado

--- src/main/java/org/springframework/samples/petclinic/service/impl/SustitucionesServiceImpl.java
+++ src/main/java/org/springframework/samples/petclinic/service/impl/SustitucionesServiceImpl.java
@@ -3,6 +3,8 @@ package org.springframework.samples.petclinic.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.samples.petclinic.model.Sustituciones;
@@ -13,7 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service("sustitucionesService")
 public class SustitucionesServiceImpl implements SustitucionesServices{
-
+	private static final Log LOG = LogFactory.getLog(SustitucionesServiceImpl.class);
+	
 	@Autowired
 	@Qualifier("sustitucionesRepository")
 	private SustitucionesRepository sustitucionesRepository;
@@ -51,5 +54,20 @@ public class SustitucionesServiceImpl implements SustitucionesServices{
 	public List<Sustituciones> findAll() {
 		// TODO Auto-generated method stub
 		return sustitucionesRepository.findAll();
+	}
+
+	@Override
+	public Sustituciones saveSustitucion(Sustituciones substitution) {
+		// TODO Auto-generated method stub
+		Sustituciones sust=sustitucionesRepository.save(substitution);
+		LOG.info(sust.toString());
+		
+		return sust;
+	}
+
+	@Override
+	public int substitutionCount() {
+		// TODO Auto-generated method stub
+		return (int) sustitucionesRepository.count();
 	}
 }
