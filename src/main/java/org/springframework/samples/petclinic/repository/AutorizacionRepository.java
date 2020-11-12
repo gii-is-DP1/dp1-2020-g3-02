358f199062655c855c7ab9e51b1320cd90d1b3a0
DiegoHill <DiegoHill@users.noreply.github.com>
2020-11-12 18:39:32 +0100

    vistasy controlador equipo

--- src/main/java/org/springframework/samples/petclinic/repository/AutorizacionRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/AutorizacionRepository.java
@@ -1,14 +1,18 @@
 package org.springframework.samples.petclinic.repository;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.samples.petclinic.enumerate.TipoAutorizacion;
 import org.springframework.samples.petclinic.model.Autorizacion;
+import org.springframework.stereotype.Repository;
 
-public interface AutorizacionRepository extends ExtendedJpaRepository<Autorizacion> {
+@Repository("autorizacionRepository")
+public interface AutorizacionRepository extends JpaRepository<Autorizacion,Serializable> {
 	
 	public List<Autorizacion> findByFecha(LocalDate date);
 	public List<Autorizacion> findByTipo(TipoAutorizacion tipo);
