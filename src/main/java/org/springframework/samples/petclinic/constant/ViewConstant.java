083cde7876c969c4668b7486d2f0400c2ed69bd5
gonlalle <gonlalalv@alum.us.es>
2020-11-16 23:01:04 +0100

    estadisticas entrenamientos

--- src/main/java/org/springframework/samples/petclinic/constant/ViewConstant.java
+++ src/main/java/org/springframework/samples/petclinic/constant/ViewConstant.java
@@ -17,4 +17,5 @@ public class ViewConstant {
 	public static final String VIEW_NAVBAR = "navbar";
 	
 	public static final String VIEW_ESTADISTICAS_JUGADOR_POR_PARTIDO = "partidos/estadisticasPersonalesPartidos";
+	public static final String VIEW_ESTADISTICAS_JUGADOR_POR_ENTRENAMIENTO = "entrenamientos/estadisticasPersonalesEntrenamiento";
 }
--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -116,12 +116,28 @@ INSERT INTO entrenadores(id,username,first_name, last_name, email, fecha_nacimie
 INSERT INTO entrenadores(id,username,first_name, last_name, email, fecha_nacimiento) VALUES (2,'bencrealc','Mauricio', 'Colmenero Muñoz', 'mastodonquijote@gmail.com','1997-8-19');
 
 
+-- INSERT  PARTIDOS
+
 INSERT INTO partidos(id,fecha,hora,tiempo_calentamiento) VALUES (1,'2020-11-06','17:00',5);
 INSERT INTO partidos(id,fecha,hora,tiempo_calentamiento) VALUES (2,'2020-11-06','18:00',5);
 INSERT INTO partidos(id,fecha,hora,tiempo_calentamiento) VALUES (3,'2020-11-06','19:00',5);
 INSERT INTO partidos(id,fecha,hora,tiempo_calentamiento) VALUES (4,'2020-12-06','17:00',5);
 INSERT INTO partidos(id,fecha,hora,tiempo_calentamiento) VALUES (5,'2020-10-06','18:00',5);
 INSERT INTO partidos(id,fecha,hora,tiempo_calentamiento) VALUES (6,'2020-11-07','19:00',5);
+
+
+-- INSERT  ENTRENAMIENTOS
+
+INSERT INTO entrenamientos(id,fecha,hora,tiempo_calentamiento) VALUES (1,'2020-11-11','17:00',5);
+INSERT INTO entrenamientos(id,fecha,hora,tiempo_calentamiento) VALUES (2,'2020-11-05','18:00',5);
+INSERT INTO entrenamientos(id,fecha,hora,tiempo_calentamiento) VALUES (3,'2020-11-07','19:00',5);
+INSERT INTO entrenamientos(id,fecha,hora,tiempo_calentamiento) VALUES (4,'2020-11-12','17:00',5);
+INSERT INTO entrenamientos(id,fecha,hora,tiempo_calentamiento) VALUES (5,'2020-11-08','18:00',5);
+INSERT INTO entrenamientos(id,fecha,hora,tiempo_calentamiento) VALUES (6,'2019-11-07','19:00',5);
+
+
+
+-- INSERT ESTADISITICAS PERSONALES PARTIDO
 
 INSERT INTO estadisticas_personal_partido(id,saques_acertados,saques_totales,porcentaje_saques,recepciones_acertadas,recepciones_totales,porcentaje_recepciones,
 	colocaciones_acertadas,colocaciones_totales,porcentaje_colocaciones,defensas_acertadas,defensas_totales,porcentaje_defensas,bloqueos_acertados,bloqueos_totales,porcentaje_bloqueos,
@@ -148,12 +164,35 @@ INSERT INTO estadisticas_personal_partido(id,saques_acertados,saques_totales,por
 	num_ataques_rapidos_acertados,num_ataques_rapidos_totales,porcentaje_ataques_rapidos,jugador_id,partido_id) VALUES (6,14,22,0.63,5,20,0.25,5,20,0.25,5,20,0.25,10,20,0.5,15,20,0.75,2,6); 
 
 
+-- INSERT ESTADISITICAS PERSONALES ENTRENAMIENTO
+
+INSERT INTO estadisticas_personal_entrenamiento(id,saques_acertados,saques_totales,porcentaje_saques,recepciones_acertadas,recepciones_totales,porcentaje_recepciones,
+	colocaciones_acertadas,colocaciones_totales,porcentaje_colocaciones,defensas_acertadas,defensas_totales,porcentaje_defensas,bloqueos_acertados,bloqueos_totales,porcentaje_bloqueos,
+	remates_acertados,remates_totales,porcentaje_remates,jugador_id,entrenamiento_id) VALUES (1,20,20,1,10,20,0.5,5,20,0.25,5,20,0.25,5,20,0.25,15,20,0.75,1,1);
+
+INSERT INTO estadisticas_personal_entrenamiento(id,saques_acertados,saques_totales,porcentaje_saques,recepciones_acertadas,recepciones_totales,porcentaje_recepciones,
+	colocaciones_acertadas,colocaciones_totales,porcentaje_colocaciones,defensas_acertadas,defensas_totales,porcentaje_defensas,bloqueos_acertados,bloqueos_totales,porcentaje_bloqueos,
+	remates_acertados,remates_totales,porcentaje_remates,jugador_id,entrenamiento_id) VALUES (2,12,18,0.6,10,20,0.5,5,20,0.25,5,20,0.25,5,20,0.25,5,20,0.25,1,2); 
+
+INSERT INTO estadisticas_personal_entrenamiento(id,saques_acertados,saques_totales,porcentaje_saques,recepciones_acertadas,recepciones_totales,porcentaje_recepciones,
+	colocaciones_acertadas,colocaciones_totales,porcentaje_colocaciones,defensas_acertadas,defensas_totales,porcentaje_defensas,
+	jugador_id,entrenamiento_id) VALUES (3,14,22,0.7,10,20,0.5,15,20,0.75,5,20,0.25,1,3);
+	
+INSERT INTO estadisticas_personal_entrenamiento(id,saques_acertados,saques_totales,porcentaje_saques,recepciones_acertadas,recepciones_totales,porcentaje_recepciones,
+	colocaciones_acertadas,colocaciones_totales,porcentaje_colocaciones,defensas_acertadas,defensas_totales,porcentaje_defensas,
+	jugador_id,entrenamiento_id) VALUES (4,10,25,0.4,10,20,0.5,5,20,0.25,15,20,0.75,1,4);
+	
+INSERT INTO estadisticas_personal_entrenamiento(id,saques_acertados,saques_totales,porcentaje_saques,recepciones_acertadas,recepciones_totales,porcentaje_recepciones,
+	colocaciones_acertadas,colocaciones_totales,porcentaje_colocaciones,defensas_acertadas,defensas_totales,porcentaje_defensas,
+	jugador_id,entrenamiento_id) VALUES (5,12,14,0.7,15,20,0.75,5,20,0.25,5,20,0.25,1,5); 
+	
+INSERT INTO estadisticas_personal_entrenamiento(id,saques_acertados,saques_totales,porcentaje_saques,recepciones_acertadas,recepciones_totales,porcentaje_recepciones,
+	colocaciones_acertadas,colocaciones_totales,porcentaje_colocaciones,defensas_acertadas,defensas_totales,porcentaje_defensas,fintas_acertadas,fintas_totales,porcentaje_fintas,
+	num_ataques_rapidos_acertados,num_ataques_rapidos_totales,porcentaje_ataques_rapidos,jugador_id,entrenamiento_id) VALUES (6,14,22,0.63,5,20,0.25,5,20,0.25,5,20,0.25,10,20,0.5,15,20,0.75,2,6); 
+
 INSERT INTO pruebas_condicion_fisica (id,dato,fecha,tipo_prueba,jugador_id) VALUES (1,3,'2020-11-06','SALTOVERTICAL',1);
 INSERT INTO pruebas_condicion_fisica (id,dato,fecha,tipo_prueba,jugador_id) VALUES (2,3,'2020-11-06','AGILIDAD',1);
 
-INSERT INTO entrenamientos(id,fecha,hora,tiempo_calentamiento) VALUES (1,'2020-11-11','17:00',5);
-INSERT INTO entrenamientos(id,fecha,hora,tiempo_calentamiento) VALUES (2,'2020-11-05','18:00',5);
-INSERT INTO entrenamientos(id,fecha,hora,tiempo_calentamiento) VALUES (3,'2020-11-07','19:00',5);
 
 INSERT INTO realiza_entrenamiento (entrenamiento_id,jugador_id) values (1,1);
 INSERT INTO realiza_entrenamiento (entrenamiento_id,jugador_id) values (2,1);
