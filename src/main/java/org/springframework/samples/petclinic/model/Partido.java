e4d0ec05066b25e3c3ebb42a195554f554d6953d
Superjaviergf <superjaviergf@gmail.com>
2020-11-18 19:34:23 +0100

    arreglo

--- src/main/java/org/springframework/samples/petclinic/model/Partido.java
+++ src/main/java/org/springframework/samples/petclinic/model/Partido.java
@@ -1,4 +1,3 @@
-
 package org.springframework.samples.petclinic.model;
 
 import java.time.LocalDate;
@@ -62,98 +61,6 @@ public class Partido extends EstadisticasEntity{
 	@Column(name = "sistema_juego", nullable = false, columnDefinition = "varchar(255) default 'CINCO_UNO'")
 	@Enumerated(value = EnumType.STRING)
 	private Sistema sistemaJuego;
-	
-	@Column(name = "saques_acertados", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int saquesAcertados;
-	
-	@Column(name = "saques_totales", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int saquesTotales;
-	
-	@Column(name = "porcentaje_saques", scale = 2, columnDefinition = "double default 0")
-	private double porcentajeSaques;
-	
-	@Column(name = "recepciones_acertadas", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int recepcionesAcertadas;
-	
-	@Column(name = "recepciones_totales", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int recepcionesTotales;
-	
-	@Column(name = "porcentaje_recepciones", scale = 2, columnDefinition = "double default 0")
-	private double porcentajeRecepciones;
-	
-	@Column(name = "colocaciones_acertadas", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int colocacionesAcertadas;
-	
-	@Column(name = "colocaciones_totales", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int colocacionesTotales;
-	
-	@Column(name = "porcentaje_colocaciones", scale = 2, columnDefinition = "double default 0")
-	private double porcentajeColocaciones;
-	
-	@Column(name = "defensas_acertadas", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int defensasAcertadas;
-	
-	@Column(name = "defensas_totales", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int defensasTotales;
-	
-	@Column(name = "porcentaje_defensas", scale = 2, columnDefinition = "double default 0")
-	private double porcentajeDefensas;
-	
-	@Column(name = "bloqueos_acertados", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int bloqueosAcertados;
-	
-	@Column(name = "bloqueos_totales", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int bloqueosTotales;
-	
-	@Column(name = "porcentaje_bloqueos", scale = 2, columnDefinition = "double default 0")
-	private double porcentajeBloqueos;
-	
-	@Column(name = "remates_acertados", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int rematesAcertados;
-	
-	@Column(name = "remates_totales", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int rematesTotales;
-	
-	@Column(name = "porcentaje_remates", scale = 2, columnDefinition = "double default 0")
-	private double porcentajeRemates;
-	
-	@Column(name = "fintas_acertadas", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int fintasAcertadas;
-	
-	@Column(name = "fintas_totales", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int fintasTotales;
-	
-	@Column(name = "porcentaje_fintas", scale = 2, columnDefinition = "double default 0")
-	private double porcentajeFintas;
-	
-	@Column(name = "num_ataques_rapidos_acertados", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int numAtaquesRapidosAcertados;
-	
-	@Column(name = "num_ataques_rapidos_totales", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int numAtaquesRapidosTotales;
-	
-	@Column(name = "porcentaje_ataques_rapidos", scale = 2, columnDefinition = "double default 0")
-	private double porcentajeAtaquesRapidos;
-	
-	@Column(name = "num_faltas_totales", nullable = false, columnDefinition = "integer default 0")
-	@Min(0)
-	private int numFaltasTotales;
 	
 	@Column(name = "num_amarillas", nullable = false, columnDefinition = "integer default 0")
 	@Min(0)
