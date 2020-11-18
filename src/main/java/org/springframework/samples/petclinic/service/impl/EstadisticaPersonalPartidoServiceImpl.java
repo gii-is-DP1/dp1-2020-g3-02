01347ff5c08da54e3594d5dc572c3797aa3fa16c
Superjaviergf <superjaviergf@gmail.com>
2020-11-18 21:55:06 +0100

    Introducción de datos y logica para traspaso de datos en estadísticas

--- src/main/java/org/springframework/samples/petclinic/service/impl/EstadisticaPersonalPartidoServiceImpl.java
+++ src/main/java/org/springframework/samples/petclinic/service/impl/EstadisticaPersonalPartidoServiceImpl.java
@@ -3,20 +3,29 @@ package org.springframework.samples.petclinic.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.samples.petclinic.model.EstadisticaPersonalPartido;
+import org.springframework.samples.petclinic.model.Jugador;
 import org.springframework.samples.petclinic.repository.EstadisticaPersonalPartidoRepository;
 import org.springframework.samples.petclinic.service.EstadisticaPersonalPartidoService;
+import org.springframework.samples.petclinic.service.JugadorService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service("estadisticaPersonalPartidoService")
 public class EstadisticaPersonalPartidoServiceImpl implements EstadisticaPersonalPartidoService{
+	
+	public static final Log LOG = LogFactory.getLog(EstadisticaPersonalEntrenamientoServiceImpl.class);
 
 	@Autowired
 	@Qualifier("estadisticaPersonalPartidoRepository")
 	private EstadisticaPersonalPartidoRepository estadisticaPersonalPartidoRepository;
+	
+	@Autowired
+	private JugadorService jugadorService;
 	
 	@Override
 	@Transactional(readOnly = true)
@@ -89,7 +98,34 @@ public class EstadisticaPersonalPartidoServiceImpl implements EstadisticaPersona
 	public EstadisticaPersonalPartido saveEstadisticaPersonalPartido(EstadisticaPersonalPartido statistic) {
 		EstadisticaPersonalPartido estadisticaPersonalPartido = estadisticaPersonalPartidoRepository.save(statistic);
 		
-		//LOG.info(estadisticaPersonalPartido.toString());
+		Jugador jugador = statistic.getJugador();
+		
+		LOG.info("JUGADOR AL QUE DEBEN ACTUALIZÁRSELES LAS ESTADÍSTICAS: "+jugador);
+		
+		/** INTRODUCCIÓN DE LAS ESTADÍSTICAS EN LAS GENERALES DEL JUGADOR */
+		jugador.setSaquesAcertados(jugador.getSaquesAcertados()+statistic.getSaquesAcertados());
+		jugador.setSaquesTotales(jugador.getSaquesTotales()+statistic.getSaquesTotales());
+		jugador.setRecepcionesAcertadas(jugador.getRecepcionesAcertadas()+statistic.getRecepcionesAcertadas());
+		jugador.setRecepcionesTotales(jugador.getRecepcionesTotales()+statistic.getRecepcionesTotales());
+		jugador.setColocacionesAcertadas(jugador.getColocacionesAcertadas()+statistic.getColocacionesAcertadas());
+		jugador.setColocacionesTotales(jugador.getColocacionesTotales()+statistic.getColocacionesTotales());
+		jugador.setDefensasAcertadas(jugador.getDefensasAcertadas()+statistic.getDefensasAcertadas());
+		jugador.setDefensasTotales(jugador.getDefensasTotales()+statistic.getDefensasTotales());
+		jugador.setBloqueosAcertados(jugador.getBloqueosAcertados()+statistic.getBloqueosAcertados());
+		jugador.setBloqueosTotales(jugador.getBloqueosTotales()+statistic.getBloqueosTotales());
+		jugador.setRematesAcertados(jugador.getRematesAcertados()+statistic.getRematesAcertados());
+		jugador.setRematesTotales(jugador.getRematesTotales()+statistic.getRematesTotales());
+		jugador.setFintasAcertadas(jugador.getFintasAcertadas()+statistic.getFintasAcertadas());
+		jugador.setFintasTotales(jugador.getFintasTotales()+statistic.getFintasTotales());
+		jugador.setNumAtaquesRapidosAcertados(jugador.getNumAtaquesRapidosAcertados()+statistic.getNumAtaquesRapidosAcertados());
+		jugador.setNumAtaquesRapidosTotales(jugador.getNumAtaquesRapidosTotales()+statistic.getNumAtaquesRapidosTotales());
+		jugador.setNumFaltasTotales(jugador.getNumFaltasTotales()+statistic.getNumFaltasTotales());
+		jugador.setNumAmarillas(jugador.getNumAmarillas()+statistic.getNumAmarillas());
+		jugador.setNumRojas(jugador.getNumRojas()+statistic.getNumRojas());
+		
+		Jugador player = jugadorService.saveJugador(jugador);
+		
+		LOG.info("JUGADOR YA ACTUALIZADO POR ESTADÍSTICAS: "+player);
 		
 		return estadisticaPersonalPartido;
 	}
