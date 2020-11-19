9cd5db13c36db54ffd514ec5c353c224f0c86532
Superjaviergf <superjaviergf@gmail.com>
2020-11-20 00:11:51 +0100

    Arreglo y mejora de EquipoRepository

--- src/main/java/org/springframework/samples/petclinic/repository/EquipoRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/EquipoRepository.java
@@ -2,6 +2,9 @@ package org.springframework.samples.petclinic.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.samples.petclinic.model.Capitan;
+import org.springframework.samples.petclinic.model.Entrenador;
 import org.springframework.samples.petclinic.model.Equipo;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +14,12 @@ public interface EquipoRepository extends ExtendedJpaRepository<Equipo> {
 	public Equipo findByCategoria(String category);
 	public List<Equipo> findByCategoriaStartingWith(String category);
 	public List<Equipo> findByLiga(String league);
+	public Equipo findByEntrenadorAndCategoria(Entrenador entrenador, String categoria);
+	public List<Equipo> findByEntrenador(Entrenador entrenador);
+	public List<Equipo> findByCapitan(Capitan capitan);
+	
+	/** POR PROBAR */
+	@Query(value = "SELECT * FROM equipos WHERE equipo_id = (SELECT equipo_id FROM perteneceA WHERE jugador_id = ?0)", nativeQuery = true)
+	public List<Equipo> findByJugador(int idJugador);
 
 }
