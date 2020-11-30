bc1e330f40e0501d6a3bbbafa95611089f0bdd99
Blancamauri <Blamaurob@alum.us.es>
2020-11-30 20:19:17 +0100

    oj

--- src/main/java/org/springframework/samples/petclinic/constant/ViewConstant.java
+++ src/main/java/org/springframework/samples/petclinic/constant/ViewConstant.java
@@ -1,40 +1,40 @@
 package org.springframework.samples.petclinic.constant;
 
 public class ViewConstant {
-	
+
 	public static final String VIEWS_EQUIPO_CREATE_OR_UPDATE_FORM = "/equipos/EquipoForm";
 	public static final String VIEW_EQUIPO = "/equipos/listadoEquipos";
-	
+
 	public static final String VIEWS_JUGADOR_CREATE_OR_UPDATE_FORM = "/jugadores/JugadorForm";
 	public static final String VIEW_JUGADOR = "/jugadores/listadoJugadores";
-	
+
 	public static final String VIEWS_PARTIDO_CREATE_OR_UPDATE_FORM = "/partidos/PartidoForm";
 	public static final String VIEW_PARTIDOS = "/partidos/listadoPartidos";
-	
+
 	public static final String VIEWS_ENTRENAMIENTO_CREATE_OR_UPDATE_FORM = "/entrenamientos/EntrenamientoForm";
 	public static final String VIEW_ENTRENAMIENTOS = "/entrenamientos/listadoEntrenamientos";
-	
+
 	public static final String VIEW_NAVBAR = "navbar";
 	public static final String VIEW_HOME = "home";
-	
+
 	public static final String VIEW_ESTADISTICAS_JUGADOR_POR_PARTIDO = "partidos/estadisticasPersonalesPartidos";
 	public static final String VIEW_ESTADISTICAS_JUGADOR_POR_ENTRENAMIENTO = "entrenamientos/estadisticasPersonalesEntrenamiento";
-	
+
 	public static final String VIEW_JUGADORES_AUTORIZACION = "/autorizaciones/listadojugadoresaut";
 	public static final String VIEW_ESTADISTICAS_PARTIDO = "partidos/estadisticasPartido";
 	public static final String VIEW_ESTADISTICAS_ENTRENAMIENTO = "entrenamientos/estadisticasEntrenamiento";
 
 	public static final String VIEW_ESTADISTICAS_PARTIDO_JUGADORES = "partidos/estadisticasPartidoTodosJugadores";
 	public static final String VIEW_ESTADISTICAS_ENTRENAMIENTO_JUGADORES = "entrenamientos/estadisticasEntrenamientoTodosJugadores";
-	
+
 	public static final String VIEW_ESTADISTICAS_PARTIDO_JUGADOR = "partidos/estadisticasPartidoJugador";
 	public static final String VIEW_ESTADISTICAS_ENTRENAMIENTO_JUGADOR = "entrenamientos/estadisticasEntrenamientoJugador";
-	
+
 	public static final String VIEW_PARTIDO_JUGADOR_POSICION = "partidos/JugadorPosicionPartido";
 	public static final String VIEW_ENTRENADOR_JUGADOR_POSICION = "entrenamientos/JugadorPosicionEntrenamiento";
-	
+
 	public static final String VIEWS_ENTRENADOR_CREATE_OR_UPDATE_FORM = "entrenadores/EntrenadorForm";
-	
 	public static final String VIEWS_ESTADISTICO_CREATE_OR_UPDATE_FORM = "estadisticos/EstadisticoForm";
+	public static final String VIEW_MATERIALES = "/materiales/listadoMateriales";
 
 }
