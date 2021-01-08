4389c4a1a200dfeee2a255434d55e02e35c628e8
DiegoHill <DiegoHill@users.noreply.github.com>
2021-01-08 15:13:15 +0100

    Eliminar jugador de equipo (no finalizado)

--- src/main/java/org/springframework/samples/petclinic/service/NumCamisetaService.java
+++ src/main/java/org/springframework/samples/petclinic/service/NumCamisetaService.java
@@ -1,6 +1,9 @@
 package org.springframework.samples.petclinic.service;
 
 import java.util.List;
+
+import org.springframework.samples.petclinic.model.Equipo;
+import org.springframework.samples.petclinic.model.Jugador;
 import org.springframework.samples.petclinic.model.NumCamiseta;
 import org.springframework.samples.petclinic.service.base.BaseService;
 
@@ -10,5 +13,6 @@ public interface NumCamisetaService extends BaseService<NumCamiseta>{
 	public abstract List<NumCamiseta> findByEquipo(int equipo_id);
 	public abstract NumCamiseta findByEquipoAndJugador(int equipo_id, int jugador_id);
 	public abstract void deleteAllInEquipo(Integer equipo_id);
+	public abstract void deleteByJugadorEquipo(Jugador jugador, Equipo equipo);
 	
 }
