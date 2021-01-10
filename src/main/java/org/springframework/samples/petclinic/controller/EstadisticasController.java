ff7dbb423a8b549c0d1cfbac5bf2e0b41d0c2609
Superjaviergf <superjaviergf@gmail.com>
2021-01-10 23:23:52 +0100

    TODO terminar comandos

--- src/main/java/org/springframework/samples/petclinic/controller/EstadisticasController.java
+++ src/main/java/org/springframework/samples/petclinic/controller/EstadisticasController.java
@@ -211,6 +211,7 @@ public class EstadisticasController {
 				} else {
 					dataActions[i].replace("%", "");
 					error = gestionErroresComandos(error, dataActions, i, dorsales, correccion);
+					//TODO
 				}
 			}
 			
