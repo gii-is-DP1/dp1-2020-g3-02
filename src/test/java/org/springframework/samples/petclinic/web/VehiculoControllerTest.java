f4dc8cd61e5ae20e92fb8e03ebe381c3255461ae
Laura Castillo Ortiz <laucasort@alum.us.es>
2021-02-02 18:33:22 +0100

    VehiculosControllerTest

--- src/test/java/org/springframework/samples/petclinic/web/VehiculoControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/VehiculoControllerTest.java
@@ -1,6 +1,8 @@
 package org.springframework.samples.petclinic.web;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -26,8 +28,35 @@ public class VehiculoControllerTest extends BaseControllerTest {
 	@WithMockUser(value = "spring")
 	@Test
 	void testListadoVehiculos() throws Exception {
+
 		mockMvc.perform(get("/personales/showvehiculos")).andExpect(status().isOk())
 		.andExpect(view().name(ViewConstant.VIEW_VEHICULO))
 		.andExpect(model().attributeExists("personales"));
+	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testPostVehiculo() throws Exception {
+	
+		mockMvc.perform(post("/personales/postvehiculo").with(csrf())
+		.param("propietario", "Pepe")
+		.param("id", "18"))
+		.andExpect(status().isCreated());
+	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testFindEditVehiculo() throws Exception {
+		
+		mockMvc.perform(get("/personales/findeditvehiculo/{id}", ID))
+		.andExpect(status().isOk());
+	}
+	
+	@WithMockUser(value = "spring")
+	@Test
+	void testEliminarVehiculo() throws Exception {
+		
+		mockMvc.perform(post("/personales/eliminarVehiculo/{id}", ID).with(csrf()))
+		.andExpect(status().isOk());
 	}
 }
