a87802196dc8ef9fc527eb5e0e296a458349abae
geron <gonlalalv@alum.us.es>
2021-02-07 14:34:57 +0100

    Propuesta A+ Ejecución Programada

--- src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
@@ -1,10 +1,12 @@
 package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication()
+@EnableScheduling
 public class PetclinicApplication {
 
 	public static void main(String[] args) {
