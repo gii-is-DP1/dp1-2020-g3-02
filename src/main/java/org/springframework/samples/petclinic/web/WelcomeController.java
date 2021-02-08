3669b327f0d1d4681f766bbf46a9ce3cc77993e5
Superjaviergf <superjaviergf@gmail.com>
2021-02-08 18:52:09 +0100

    Cerrar sesion

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -2,16 +2,17 @@ package org.springframework.samples.petclinic.web;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.repository.UserRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.samples.petclinic.constant.ViewConstant;
-import org.springframework.samples.petclinic.model.User;
 import org.springframework.samples.petclinic.model.padres.Person;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -67,6 +68,15 @@ public class WelcomeController {
 	  @GetMapping("/login")
 	  public String login() {
 		  return ViewConstant.VIEW_LOGIN;
+	  }
+	  
+	  @PostMapping("/logout")
+	  public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	      if (auth != null){    
+	          new SecurityContextLogoutHandler().logout(request, response, auth);
+	      }
+	      return "redirect:/login";
 	  }
 	  
 	  @GetMapping("/home")
