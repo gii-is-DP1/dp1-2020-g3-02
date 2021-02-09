d673d8b5de856ca5d0b07a20143e3f45b9f40ad3
Superjaviergf <superjaviergf@gmail.com>
2021-02-09 21:34:13 +0100

    Arreglo de porcentajes

--- src/main/java/org/springframework/samples/petclinic/service/impl/EstadisticaPersonalPartidoServiceImpl.java
+++ src/main/java/org/springframework/samples/petclinic/service/impl/EstadisticaPersonalPartidoServiceImpl.java
@@ -66,14 +66,46 @@ public class EstadisticaPersonalPartidoServiceImpl extends AbstractEstadisticasS
 	public EstadisticaPersonalPartido save(EstadisticaPersonalPartido statistic) {
 		
 		
-		if(statistic.getSaquesTotales() > 0) statistic.setPorcentajeSaques(1.*statistic.getSaquesAcertados()/statistic.getSaquesTotales());
-		if(statistic.getRecepcionesTotales() > 0) statistic.setPorcentajeRecepciones(1.*statistic.getRecepcionesAcertadas()/statistic.getRecepcionesTotales());
-		if(statistic.getColocacionesTotales() > 0) statistic.setPorcentajeColocaciones(1.*statistic.getColocacionesAcertadas()/statistic.getColocacionesTotales());
-		if(statistic.getDefensasTotales() > 0) statistic.setPorcentajeDefensas(1.*statistic.getDefensasAcertadas()/statistic.getDefensasTotales());
-		if(statistic.getBloqueosTotales() > 0) statistic.setPorcentajeBloqueos(1.*statistic.getBloqueosAcertados()/statistic.getBloqueosTotales());
-		if(statistic.getRematesTotales() > 0) statistic.setPorcentajeRemates(1.*statistic.getRematesAcertados()/statistic.getRematesTotales());
-		if(statistic.getFintasTotales() > 0) statistic.setPorcentajeFintas(1.*statistic.getFintasAcertadas()/statistic.getFintasTotales());
-		if(statistic.getNumAtaquesRapidosTotales() > 0) statistic.setPorcentajeAtaquesRapidos(1.*statistic.getNumAtaquesRapidosAcertados()/statistic.getNumAtaquesRapidosTotales());
+		if(statistic.getSaquesTotales() > 0) {
+			statistic.setPorcentajeSaques(1.*statistic.getSaquesAcertados()/statistic.getSaquesTotales());
+		} else {
+			statistic.setPorcentajeSaques(0);
+		}
+		if(statistic.getRecepcionesTotales() > 0) {
+			statistic.setPorcentajeRecepciones(1.*statistic.getRecepcionesAcertadas()/statistic.getRecepcionesTotales());
+		} else {
+			statistic.setPorcentajeRecepciones(0);
+		}
+		if(statistic.getColocacionesTotales() > 0) {
+			statistic.setPorcentajeColocaciones(1.*statistic.getColocacionesAcertadas()/statistic.getColocacionesTotales());
+		} else {
+			statistic.setPorcentajeColocaciones(0);
+		}
+		if(statistic.getDefensasTotales() > 0) {
+			statistic.setPorcentajeDefensas(1.*statistic.getDefensasAcertadas()/statistic.getDefensasTotales());
+		} else {
+			statistic.setPorcentajeDefensas(0);
+		}
+		if(statistic.getBloqueosTotales() > 0) {
+			statistic.setPorcentajeBloqueos(1.*statistic.getBloqueosAcertados()/statistic.getBloqueosTotales());
+		} else {
+			statistic.setPorcentajeBloqueos(0);
+		}
+		if(statistic.getRematesTotales() > 0) {
+			statistic.setPorcentajeRemates(1.*statistic.getRematesAcertados()/statistic.getRematesTotales());
+		} else {
+			statistic.setPorcentajeRemates(0);
+		}
+		if(statistic.getFintasTotales() > 0) {
+			statistic.setPorcentajeFintas(1.*statistic.getFintasAcertadas()/statistic.getFintasTotales());
+		} else {
+			statistic.setPorcentajeFintas(0);
+		}
+		if(statistic.getNumAtaquesRapidosTotales() > 0) {
+			statistic.setPorcentajeAtaquesRapidos(1.*statistic.getNumAtaquesRapidosAcertados()/statistic.getNumAtaquesRapidosTotales());
+		} else {
+			statistic.setPorcentajeAtaquesRapidos(0);
+		}
 		
 		EstadisticaPersonalPartido estadisticaPersonalPartido = estadisticaPersonalPartidoRepository.save(statistic);
 		return estadisticaPersonalPartido;
